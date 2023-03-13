890268860c466ccebc13e97161e7e0b12767abcc
anus420 <126487119+anus420@users.noreply.github.com>
2023-03-13 15:59:55 +0000

    lesson 2

---
+++ test/Assets/Scripts/ScheneChange.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScheneChange : MonoBehaviour
+{
+
+    public void ChangeToScene(string TargetSceneName)
+    {
+
+    }
+}
+
