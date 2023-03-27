17558e30f7cef0908577b88bd836f894d32118da
anus420 <126487119+anus420@users.noreply.github.com>
2023-03-27 15:56:04 +0100

    jump and things

--- test/Assets/Scripts/PlayerMovement.cs
+++ test/Assets/Scripts/PlayerMovement.cs
@@ -9,17 +9,31 @@ public class PlayerMovement : MonoBehaviour
 {
     //referance to a Rigidbody 2d
     private Rigidbody2D physicsBody = null;
-
+    public float jumpSpeed = 10;
+    public float playerspeed = 1;
     public void MoveLeft()
     {
         Vector2 newVelocity = physicsBody.velocity;
 
-        newVelocity.x = -1;
+        newVelocity.x = -playerspeed;
         physicsBody.velocity = newVelocity;
     }
 
+    public void MoveRight()
+    {
+        Vector2 newVelocity = physicsBody.velocity;
 
+        newVelocity.x = playerspeed;
+        physicsBody.velocity = newVelocity;
+    }
 
+    public void Jump()
+    {
+        //jump speed setting
+        Vector2 newVelocity = physicsBody.velocity;
+        newVelocity.y = jumpSpeed;
+        physicsBody.velocity = newVelocity;     
+    }
     private void Awake()
     {
         physicsBody = GetComponent<Rigidbody2D>();
